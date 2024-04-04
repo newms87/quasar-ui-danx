aa0dbb2c1db6a26d143d612a38ce1368c8e1a62d
Daniel Newman <dan@flytedesk.com>
2024-04-04 01:20:46 -0300

    adding export button

---
+++ ui/src/components/Utility/Buttons/ExportButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <QBtn class="bg-neutral-plus-6" :loading="isExporting" @click="onExport">
+    <ExportIcon class="w-5" />
+  </QBtn>
+</template>
+<script setup>
+import { DownloadIcon as ExportIcon } from '@heroicons/vue/solid';
+import { ref } from 'vue';
+import { FlashMessages } from '../../../helpers';
+
+const props = defineProps({
+  exporter: {
+    type: Function,
+    required: true
+  }
+});
+const isExporting = ref(false);
+async function onExport() {
+  isExporting.value = true;
+  try {
+    await props.exporter();
+  } catch (error) {
+    console.error(error);
+    FlashMessages.error('Failed to export data');
+  }
+  isExporting.value = false;
+}
+</script>

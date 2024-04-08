8f383313e309dcccf76c3674b83849b611307953
Daniel Newman <dan@flytedesk.com>
2024-04-08 16:41:12 -0300

    LabelValueFormat

---
+++ ui/src/components/Utility/Formats/LabelValueFormat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="text-xs font-bold">{{ label }}</div>
+    <div :class="{'mt-2': !dense, 'mt-1': dense, 'text-no-wrap': nowrap}">
+      <slot>{{ value || "-" }}</slot>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: [String, Number],
+    default: "-"
+  },
+  dense: Boolean,
+  nowrap: Boolean
+});
+</script>

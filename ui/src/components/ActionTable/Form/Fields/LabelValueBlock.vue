980e35c5300a19e643922f6c82b0eee7a7c72d10
Daniel Newman <dan@flytedesk.com>
2024-06-06 02:56:18 -0300

    Adding URL to LabelValueBlock

--- ui/src/components/ActionTable/Form/Fields/LabelValueBlock.vue
+++ ui/src/components/ActionTable/Form/Fields/LabelValueBlock.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
-    <div class="text-xs font-bold">{{ label }}</div>
-    <div :class="{'mt-2': !dense, 'mt-1': dense, 'text-no-wrap': nowrap}">
-      <slot>{{ value || "-" }}</slot>
+    <div class="text-xs font-bold">
+      {{ label }}
+    </div>
+    <div :class="valueClass">
+      <a
+        v-if="url"
+        target="_blank"
+        :href="url"
+        :class="valueClass"
+      >
+        <slot>{{ value || "-" }}</slot>
+      </a>
+      <template v-else>
+        <slot>{{ value || "-" }}</slot>
+      </template>
     </div>
   </div>
 </template>
-<script setup>
-defineProps({
-  label: {
-    type: String,
-    required: true
-  },
-  value: {
-    type: [String, Number],
-    default: "-"
-  },
-  dense: Boolean,
-  nowrap: Boolean
+<script setup lang="ts">
+import { computed } from "vue";
+
+export interface LabelValueBlockProps {
+	label: string;
+	value: string | number | boolean;
+	url?: string;
+	dense?: boolean;
+	nowrap?: boolean;
+}
+
+const props = withDefaults(defineProps<LabelValueBlockProps>(), {
+	value: "",
+	url: ""
 });
+
+const valueClass = computed(() => ({ "mt-2": !props.dense, "mt-1": props.dense, "text-no-wrap": props.nowrap }));
 </script>

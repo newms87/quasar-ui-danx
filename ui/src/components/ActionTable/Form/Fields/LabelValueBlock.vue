b8eae79a572de4d7896031ad50fe01a8bf0ab762
Daniel Newman <dan@flytedesk.com>
2024-06-06 17:38:16 -0300

    short formats (currency, number, size)

--- ui/src/components/ActionTable/Form/Fields/LabelValueBlock.vue
+++ ui/src/components/ActionTable/Form/Fields/LabelValueBlock.vue
@@ -10,16 +10,17 @@
         :href="url"
         :class="valueClass"
       >
-        <slot>{{ value || "-" }}</slot>
+        <slot>{{ formattedValue }}</slot>
       </a>
       <template v-else>
-        <slot>{{ value || "-" }}</slot>
+        <slot>{{ formattedValue }}</slot>
       </template>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { computed } from "vue";
+import { fBoolean, fNumber } from "../../../../helpers";
 
 export interface LabelValueBlockProps {
 	label: string;
@@ -35,4 +36,16 @@ const props = withDefaults(defineProps<LabelValueBlockProps>(), {
 });
 
 const valueClass = computed(() => ({ "mt-2": !props.dense, "mt-1": props.dense, "text-no-wrap": props.nowrap }));
+const formattedValue = computed(() => {
+	switch (typeof props.value) {
+		case "boolean":
+			return fBoolean(props.value);
+
+		case "number":
+			return fNumber(props.value);
+
+		default:
+			return props.value || "-";
+	}
+});
 </script>

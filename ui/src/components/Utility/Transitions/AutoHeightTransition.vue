e845651f66f034a25d0b12c2e383e234c010164a
Daniel Newman <dan@flytedesk.com>
2024-07-18 11:00:08 -0600

    WIP auto-height transition

---
+++ ui/src/components/Utility/Transitions/AutoHeightTransition.vue
@@ -0,0 +1,21 @@
+<template>
+  <Transition name="autoHeight">
+    <slot />
+  </Transition>
+</template>
+
+<style lang="scss">
+.autoHeight-enter-active,
+.autoHeight-leave-active {
+	transition: height .3s linear;
+	height: calc-size(auto);
+}
+
+.autoHeight-enter-to, .autoHeight-leave-from {
+	height: calc-size(auto);
+}
+
+.autoHeight-enter-from, .autoHeight-leave-to {
+	height: 0;
+}
+</style>

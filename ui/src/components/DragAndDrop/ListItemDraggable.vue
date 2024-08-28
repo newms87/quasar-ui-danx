f1b8d6b49175bbdae0240173acadeaa825bb8b01
Daniel Newman <dan@flytedesk.com>
2024-08-28 14:22:46 -0600

    ts updates

--- ui/src/components/DragAndDrop/ListItemDraggable.vue
+++ ui/src/components/DragAndDrop/ListItemDraggable.vue
@@ -6,7 +6,10 @@
     @dragend="dragAndDrop.dragEnd"
   >
     <div class="flex items-center">
-      <div v-if="showHandle">
+      <div
+        v-if="showHandle"
+        :class="handleClass"
+      >
         <SvgImg
           :svg="DragHandleIcon"
           class="w-4 h-4"
@@ -19,39 +22,35 @@
     </div>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import { DragHandleDotsIcon as DragHandleIcon } from "../../svg";
 import { SvgImg } from "../Utility";
 import { ListDragAndDrop } from "./listDragAndDrop";
 
 const emit = defineEmits(["position", "update:list-items"]);
-const props = defineProps({
-  dropZone: {
-    type: [Function, String],
-    required: true
-  },
-  direction: {
-    type: String,
-    default: "vertical",
-    validator: (value) => ["vertical", "horizontal"].includes(value)
-  },
-  showHandle: Boolean,
-  listItems: {
-    type: Array,
-    default: null
-  }
+const props = withDefaults(defineProps<{
+	dropZone: string | (() => string);
+	direction?: "vertical" | "horizontal";
+	showHandle?: boolean;
+	handleClass?: string | object;
+	listItems?: any[];
+}>(), {
+	direction: "vertical",
+	showHandle: true,
+	handleClass: "",
+	listItems: () => []
 });
 
 const dragAndDrop = new ListDragAndDrop()
-  .setDropZone(props.dropZone)
-  .setOptions({ showPlaceholder: true, direction: props.direction })
-  .onPositionChange((newPosition, oldPosition) => {
-    emit("position", newPosition);
+	.setDropZone(props.dropZone)
+	.setOptions({ showPlaceholder: true, direction: props.direction })
+	.onPositionChange((newPosition, oldPosition) => {
+		emit("position", newPosition);
 
-    if (props.listItems) {
-      const items = [...props.listItems];
-      items.splice(newPosition, 0, items.splice(oldPosition, 1)[0]);
-      emit("update:list-items", items);
-    }
-  });
+		if (props.listItems) {
+			const items = [...props.listItems];
+			items.splice(newPosition, 0, items.splice(oldPosition, 1)[0]);
+			emit("update:list-items", items);
+		}
+	});
 </script>

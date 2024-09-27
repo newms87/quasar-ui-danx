6338ca39280979d6be0f0fd7b190228325140333
Daniel Newman <dan@flytedesk.com>
2024-09-27 14:33:09 -0400

    Handling geolocation more robustly

--- ui/src/components/ActionTable/Form/Fields/SelectDrawer.vue
+++ ui/src/components/ActionTable/Form/Fields/SelectDrawer.vue
@@ -69,74 +69,74 @@ import { ContentDrawer } from "../../../Utility";
 
 const emit = defineEmits(["update:modelValue"]);
 const props = defineProps({
-  modelValue: {
-    type: [Object, String, Array, null],
-    required: true
-  },
-  options: {
-    type: Array,
-    default: () => []
-  },
-  multiple: Boolean,
-  label: {
-    type: String,
-    default: "Select"
-  },
-  placeholder: {
-    type: String,
-    default: "All"
-  }
+	modelValue: {
+		type: [Object, String, Array, null],
+		required: true
+	},
+	options: {
+		type: Array,
+		default: () => []
+	},
+	multiple: Boolean,
+	label: {
+		type: String,
+		default: "Select"
+	},
+	placeholder: {
+		type: String,
+		default: "All"
+	}
 });
 
 const showDrawer = ref(false);
 const formattedOptions = computed(() =>
-  props.options.map((opt) =>
-    typeof opt === "string"
-      ? {
-        label: opt,
-        value: opt
-      }
-      : opt
-  )
+	props.options.map((opt) =>
+		typeof opt === "string"
+			? {
+				label: opt,
+				value: opt
+			}
+			: opt
+	)
 );
 
 function getOptionValue(option) {
-  return option.value === undefined ? option : option.value;
+	return option.value === undefined ? option : option.value;
 }
 
 function getOptionLabel(value) {
-  return formattedOptions.value.find((opt) => opt.value === value).label;
+	return formattedOptions.value.find((opt) => opt.value === value)?.label;
 }
 
 function isSelected(option) {
-  const optionValue = getOptionValue(option);
+	const optionValue = getOptionValue(option);
 
-  if (props.multiple) {
-    return props.modelValue.includes(optionValue);
-  } else {
-    return props.modelValue === optionValue;
-  }
+	if (props.multiple) {
+		return props.modelValue.includes(optionValue);
+	} else {
+		return props.modelValue === optionValue;
+	}
 }
 
 function toggleSelect(option) {
-  let optionValue = getOptionValue(option);
+	let optionValue = getOptionValue(option);
 
-  let selection = optionValue;
+	let selection = optionValue;
 
-  if (props.multiple) {
-    selection = [...props.modelValue];
-    if (isSelected(optionValue)) {
-      selection = selection.filter((opt) => opt !== optionValue);
-    } else {
-      selection.push(optionValue);
-    }
-  } else {
-    // Allow deselection on single select
-    if (selection === props.modelValue) {
-      selection = null;
-    }
-  }
+	if (props.multiple) {
+		selection = [...props.modelValue];
+		if (isSelected(optionValue)) {
+			selection = selection.filter((opt) => opt !== optionValue);
+		} else {
+			selection.push(optionValue);
+		}
+	} else {
+		// Allow deselection on single select
+		if (selection === props.modelValue) {
+			selection = null;
+		}
+	}
 
-  emit("update:modelValue", selection);
+	emit("update:modelValue", selection);
 }
 </script>

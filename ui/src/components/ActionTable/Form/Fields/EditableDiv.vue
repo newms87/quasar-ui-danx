12c6e28ffea785e2db1d9e54714f9d8f5d7f4657
Daniel Newman <dan@flytedesk.com>
2024-11-21 18:40:32 -0300

    EditableDiv customizable colors + placeholder

--- ui/src/components/ActionTable/Form/Fields/EditableDiv.vue
+++ ui/src/components/ActionTable/Form/Fields/EditableDiv.vue
@@ -1,29 +1,55 @@
 <template>
-  <div
-    contenteditable
-    class="inline-block transition duration-300 outline-none outline-offset-0"
-    :class="contentClass"
-    @input="onInput"
-  >
-    {{ text }}
+  <div class="inline-block relative">
+    <div
+      contenteditable
+      class="relative inline-block transition duration-300 outline-none outline-offset-0 border-none focus:outline-4 hover:outline-4 rounded-sm z-10"
+      :style="{minWidth, minHeight}"
+      :class="contentClass"
+      @input="onInput"
+    >
+      {{ text }}
+    </div>
+    <div
+      v-if="!text && placeholder"
+      ref="placeholderDiv"
+      class="text-gray-600 absolute-top-left whitespace-nowrap z-1"
+    >
+      {{ placeholder }}
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useDebounceFn } from "@vueuse/core";
-import { ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 const emit = defineEmits(["update:model-value", "change"]);
 const props = withDefaults(defineProps<{
 	modelValue: string;
-	contentClass?: string;
+	color?: string;
 	debounceDelay?: number;
+	placeholder?: string;
 }>(), {
-	contentClass: "hover:bg-blue-200 focus:bg-blue-200",
-	debounceDelay: 1000
+	// NOTE: You must safe-list required colors in tailwind.config.js
+	//       Add text-blue-900, hover:bg-blue-200, hover:outline-blue-200, focus:outline-blue-200 and focus:bg-blue-200 for the following config
+	color: "blue-200",
+	textColor: "blue-900",
+	debounceDelay: 1000,
+	placeholder: "Enter Text..."
 });
 
 const text = ref(props.modelValue);
+const placeholderDiv = ref(null);
+const minWidth = ref(0);
+const minHeight = ref(0);
+
+onMounted(() => {
+	// Set the min-width to the width of the placeholder
+	if (placeholderDiv.value) {
+		minWidth.value = placeholderDiv.value.offsetWidth + "px";
+		minHeight.value = placeholderDiv.value.offsetHeight + "px";
+	}
+});
 
 const debouncedChange = useDebounceFn(() => {
 	emit("update:model-value", text.value);
@@ -35,4 +61,9 @@ function onInput(e) {
 	debouncedChange();
 }
 
+const contentClass = computed(() => [
+	`hover:bg-${props.color} focus:bg-${props.color}`,
+	`hover:text-${props.textColor} focus:text-${props.textColor}`,
+	`hover:outline-${props.color} focus:outline-${props.color}`
+]);
 </script>

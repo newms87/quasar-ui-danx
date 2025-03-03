13ce55acc4ccd61a3b06947db1deaf7861c2dc29
Daniel Newman <dan@flytedesk.com>
2025-03-03 15:42:24 -0300

    CollapsableSidebar toggleClass updates

--- ui/src/components/Utility/Layouts/CollapsableSidebar.vue
+++ ui/src/components/Utility/Layouts/CollapsableSidebar.vue
@@ -15,92 +15,82 @@
       <div
         v-if="!toggleAtTop"
         class="flex w-full p-4"
-        :class="rightSide ? 'justify-start' : 'justify-end'"
+        :class="{'justify-start': rightSide, 'justify-end': !rightSide, ...resolveToggleClass}"
       >
         <slot name="toggle">
           <QBtn
             class="btn-secondary"
             @click="toggleCollapse"
           >
-            <ToggleIcon
-              class="w-5 transition-all"
-              :class="{ 'rotate-180': rightSide ? !isCollapsed : isCollapsed }"
-            />
+            <ToggleIcon :class="{ 'rotate-180': rightSide ? !isCollapsed : isCollapsed, ...resolvedToggleIconClass }" />
           </QBtn>
         </slot>
       </div>
       <div
         v-else
         class="absolute top-0 right-0 cursor-pointer p-2"
-        :class="toggleClass"
+        :class="resolveToggleClass"
         @click="toggleCollapse"
       >
-        <ToggleIcon
-          class="w-5 transition-all"
-          :class="{ 'rotate-180': rightSide ? !isCollapsed : isCollapsed }"
-        />
+        <ToggleIcon :class="{ 'rotate-180': rightSide ? !isCollapsed : isCollapsed, ...resolvedToggleIconClass }" />
       </div>
     </template>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import { ChevronLeftIcon as ToggleIcon } from "@heroicons/vue/outline";
 import { computed, onMounted, ref, watch } from "vue";
 import { getItem, setItem } from "../../../helpers";
+import { AnyObject } from "../../../types";
 
 const emit = defineEmits(["collapse", "update:collapse"]);
-const props = defineProps({
-  rightSide: Boolean,
-  displayClass: {
-    type: String,
-    default: "flex flex-col"
-  },
-  maxWidth: {
-    type: String,
-    default: "13.5rem"
-  },
-  minWidth: {
-    type: String,
-    default: "5.5rem"
-  },
-  disabled: Boolean,
-  collapse: Boolean,
-  name: {
-    type: String,
-    default: "sidebar"
-  },
-  toggleAtTop: Boolean,
-  toggleClass: {
-    type: String,
-    default: ""
-  },
-  hideToggleOnCollapse: Boolean
+const props = withDefaults(defineProps<{
+	displayClass?: string;
+	toggleClass?: string | AnyObject;
+	toggleIconClass?: string | AnyObject;
+	rightSide?: boolean;
+	maxWidth?: string;
+	minWidth?: string;
+	disabled?: boolean;
+	collapse?: boolean;
+	name?: string;
+	toggleAtTop?: boolean;
+	hideToggleOnCollapse?: boolean;
+}>(), {
+	displayClass: "flex flex-col",
+	maxWidth: "13.5rem",
+	minWidth: "5.5rem",
+	name: "sidebar",
+	toggleClass: "",
+	toggleIconClass: "w-5 transition-all"
 });
 
 const isCollapsed = ref(getItem(props.name + "-is-collapsed", props.collapse));
 
 function setCollapse(state) {
-  isCollapsed.value = state;
-  setItem(props.name + "-is-collapsed", !!isCollapsed.value);
+	isCollapsed.value = state;
+	setItem(props.name + "-is-collapsed", !!isCollapsed.value);
 }
 
 function toggleCollapse() {
-  setCollapse(!isCollapsed.value);
-  emit("collapse", isCollapsed.value);
-  emit("update:collapse", isCollapsed.value);
+	setCollapse(!isCollapsed.value);
+	emit("collapse", isCollapsed.value);
+	emit("update:collapse", isCollapsed.value);
 }
 
 onMounted(() => {
-  emit("collapse", isCollapsed.value);
-  emit("update:collapse", isCollapsed.value);
+	emit("collapse", isCollapsed.value);
+	emit("update:collapse", isCollapsed.value);
 });
 const style = computed(() => {
-  return {
-    width: isCollapsed.value ? props.minWidth : props.maxWidth
-  };
+	return {
+		width: isCollapsed.value ? props.minWidth : props.maxWidth
+	};
 });
 
+const resolveToggleClass = computed(() => typeof props.toggleClass === "string" ? { [props.toggleClass]: true } : props.toggleClass);
+const resolvedToggleIconClass = computed(() => typeof props.toggleIconClass === "string" ? { [props.toggleIconClass]: true } : props.toggleIconClass);
 watch(() => props.collapse, () => {
-  setCollapse(props.collapse);
+	setCollapse(props.collapse);
 });
 </script>
